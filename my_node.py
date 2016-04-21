e0a07fb58e0818a8804f371b62a92e8969b7565a
wolf1996 <ksgiv37@gmail.com>
2016-04-21 23:45:37 +0300

    creating node dictionary

--- my_node.py
+++ my_node.py
@@ -5,11 +5,14 @@
 from rdflib import Graph
 from rdflib.namespace import RDF, RDFS
 from rdflib import Namespace
+from neo4jrestclient.client import GraphDatabase as GDB
+
 
 class NodeContainer:
     """
         Node data container
     """
+
     def __init__(self, name=None):
         """
             init function
@@ -26,13 +29,19 @@ class NodeContainer:
         """
         self.__name = name
 
+    def set_uri(self, uri):
+        """
+            Set's URI to object
+        """
+        self.__uri = uri
+
     def add_props(self, prop_update):
         """
             Add properties to props dictionary
         """
         self.__props.update(prop_update)
 
-    def add_labels(self, label_update):
+    def add_label(self, label_update):
         """
             Add labels to labels array
         """
@@ -51,27 +60,46 @@ class NodeContainer:
         """
         return self.__props
 
+    def __str__(self):
+        """
+            convert class to string
+        """
+        name_str = "node name is %s\n" % self.__name
+        label_str = "labels are %s\n" % str(self.__labels)
+        propety_str = "properties are %s\n" % str(self.__props)
+        return name_str + label_str + propety_str
 
-def rdf_loader(gdb,rdf):
+
+def rdf_loader(gdb, rdf):
     """
         Get Neo4j connection and rdf file
     """
     owl = Namespace("http://www.w3.org/2002/07/owl#")
     node_dict = {}
-    for j in g.subjects(predicate=RDF.type,object=OWL.Class):
+    for j in rdf.subjects(predicate=RDF.type, object=owl.Class):
         jdata = (j.split('#')[-1])
-        for i in g.subjects(object=j,predicate=RDF.type):
+        for i in rdf.subjects(object=j, predicate=RDF.type):
             idata = i.split('#')[-1]
             if idata in node_dict.keys():
-                node_dict
-
+                node_dict[idata].add_label(jdata)
+            else:
+                buf_node = NodeContainer(idata)
+                buf_node.add_label(jdata)
+                buf_node.set_uri(i)
+                node_dict.update({idata: buf_node})
+    for i in node_dict.keys():
+        print("%s %s" % (i, node_dict[i],))
 
 
 def test():
     """
        some tests
     """
-    print("thera are no tests... yet")
+    rdf = Graph()
+    rdf.parse("RDF2.owl", 'application/rdf+xml')
+    gdb = GDB("http://localhost:7474/db/data",
+              username='neo4j', password='123456')
+    rdf_loader(gdb, rdf)
 
 
 if __name__ == '__main__':
