6a856ae493421f0976e332353991fe92c7a67d4f
wolf1996 <ksgiv37@gmail.com>
2016-06-06 01:59:34 +0300

    get structure of objects

--- classes.py
+++ classes.py
@@ -4,9 +4,8 @@
 """
 
 from nltk.parse.stanford import StanfordDependencyParser
-from nltk import Tree, ProbabilisticTree
-from nltk.internals import find_jars_within_path
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem import SnowballStemmer
+
 
 class Film:
     """
@@ -14,7 +13,9 @@ class Film:
     """
     synonims = ["films", "film", "picture", ]
     labels = ["horror", "romantic", "thriller", "comedy", ]
-    stemmer = LancasterStemmer()
+    properties = ["release", ]
+    stemmer = SnowballStemmer("english")
+
     @staticmethod
     def valid_name(word):
         """
@@ -24,13 +25,37 @@ class Film:
         return (word in Film.synonims) or (word in Film.labels)
 
     def __init__(self, name):
+        self.__name = name
+        self.__stemname = Film.stemmer.stem(name)
+        self.__labellist = []
         return
 
     def get_info(self, triples, objects):
         """
             getting info from triples
         """
+        for i in triples:
+            if i[0][0] == self.__name:
+                dname = i[2][0]
+                stemdname = Film.stemmer.stem(dname)
+                if stemdname in Film.labels:
+                    self.__labellist.append(stemdname)
+                    objects.update({stemdname: None})
+                if Connection.valid_name(stemdname):
+                    if stemdname in objects.keys():
+                        con = objects[stemdname]
+                        con.set_source(self)
+                        print("!!!!!!!!!!!!!lready in list!!!!!!!!!!!!!!!!!!!!")
+                    else:
+                        con = Connection(stemdname)
+                        con.set_source(self)
+                        objects.update({stemdname: con})
+                        con.get_info(triples, objects)
+                        objects[stemdname] = con
         return
+
+    def __str__(self):
+        return self.__name + '\t' + self.__stemname
 
 
 class Person:
@@ -39,42 +64,117 @@ class Person:
     """
     synonims = ["person", "persons", "men", ]
     labels = ["actor", "director", ]
-    stemmer = LancasterStemmer()
+    properties = ["born", ]
+    stemmer = SnowballStemmer("english")
+
     @staticmethod
     def valid_name(word):
         """
             check if word means person
         """
-        return
+        word = Person.stemmer.stem(word)
+        return (word in Person.synonims) or (word in Person.labels)
 
     def __init__(self, name):
+        self.__name = name
+        self.__stemname = Person.stemmer.stem(name)
+        self.__labellist = []
         return
+
     def get_info(self, triples, objects):
         """
             getting info from triples
         """
-        word = Person.stemmer.stem(word)
-        return (word in Person.synonims) or (word in Person.labels)
+        for i in triples:
+            if i[0][0] == self.__name:
+                dname = i[2][0]
+                stemdname = Person.stemmer.stem(dname)
+                if stemdname in Person.labels:
+                    self.__labellist.append(stemdname)
+                    objects.update({dname: None})
+        return
+
+    def __str__(self):
+        return self.__name + '\t' + self.__stemname
 
 
 class Connection:
     """
         class contain info about connections
     """
-    labels = ["played", "acted", "directed", ]
-    stemmer = LancasterStemmer()
+    labels = ["play", "act", "direct", ]
+    stemmer = SnowballStemmer("english")
+
     @staticmethod
     def valid_name(word):
         """
             check if word means Connection
         """
         word = Connection.stemmer.stem(word)
-        return (word in Connection.labels)
+        return word in Connection.labels
 
     def __init__(self, name):
+        self.__name = name
+        self.__stemname = Connection.stemmer.stem(name)
+        self.source = []
+        self.dest = []
         return
+
     def get_info(self, triples, objects):
+        """
+            getting info from triples
+        """
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        for i in triples:
+            if i[0][0] == self.__name:
+                dname = i[2][0]
+                stemdname = Connection.stemmer.stem(dname)
+                if Person.valid_name(dname):
+                    obj = Person(stemdname)
+                    objects.update({dname: obj})
+                    obj.get_info(triples, objects)
+                    objects[dname] = obj
+                    self.set_dest(obj)
+                    print('!!!!!!!!!!!!!!!!!!!!OBJECT ADDED!!!!!!!!!!!!!!!!!!!!!')
+                if Film.valid_name(dname):
+                    obj = Film(stemdname)
+                    objects.update({dname: obj})
+                    obj.get_info(triples, objects)
+                    objects[dname] = obj
+                    self.set_dest(obj)
+                    print('!!!!!!!!!!!!!!!!!!!!OBJECT ADDED!!!!!!!!!!!!!!!!!!!!!')
+                if Myobject.valid_name(dname):
+                    obj = Myobject(stemdname)
+                    objects.update({dname: obj})
+                    obj.get_info(triples, objects)
+                    objects[dname] = obj
+                    self.set_dest(obj)
+                    print('!!!!!!!!!!!!!!!!!!!!OBJECT ADDED!!!!!!!!!!!!!!!!!!!!!')
         return
+
+    def set_source(self, obj):
+        """
+            adding first node of connection
+        """
+        self.source.append(obj)
+
+    def set_dest(self, obj):
+        """
+            adding second node of connection
+        """
+        self.dest.append(obj)
+
+    def __str__(self):
+        rstr = ''
+        rstr += '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+        rstr += self.__name + '\n'
+        rstr += self.__stemname + '\n'
+        rstr += 'sources:' + '\n'
+        rstr += str([str(i) for i in self.source]) + '\n'
+        rstr += 'dests:' + '\n'
+        rstr += str([str(i) for i in self.dest]) + '\n'
+        rstr += '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + '\n'
+        return rstr
 
 
 class Myobject:
@@ -82,16 +182,17 @@ class Myobject:
         objects class
     """
     objlist = []
-    stemmer = LancasterStemmer()
+    stemmer = SnowballStemmer("english")
+
     @staticmethod
     def valid_name(word):
         """
             check if word means named objects
         """
-        word = Myobject.stemmer.stem(word)
         return (word in Myobject.objlist) or word.isdigit()
 
     def __init__(self, name):
+        self.__name = name
         return
 
     @staticmethod
@@ -121,6 +222,15 @@ class Myobject:
             rstr += i
         return rstr
 
+    def get_info(self, triples, objects):
+        """
+            getting info from triples
+        """
+        return
+
+    def __str__(self):
+        return self.__name
+
 
 def tree_parser(triples):
     """
@@ -128,6 +238,7 @@ def tree_parser(triples):
     """
     lst = []
     return lst
+
 
 def get_obj(triples):
     """
@@ -146,12 +257,27 @@ def get_obj(triples):
             continue
         for mclass in classlist:
             if mclass.valid_name(name):
-                print("name valid: "+name)
+                print("name valid: " + name)
                 obj = mclass(name)
-                objlist.update({name:obj})
+                objlist.update({name: obj})
+                obj.get_info(triples, objlist)
+                objlist[name] = obj
+    for i in triples:
+        name = i[2][0]
+        print("name: " + name)
+        if name in objlist.keys():
+            continue
+        for mclass in classlist:
+            if mclass.valid_name(name):
+                print("name valid: " + name)
+                obj = mclass(name)
+                objlist.update({name: obj})
+                obj.get_info(triples, objlist)
+                objlist[name] = obj
     print(objlist.keys())
+    for i in objlist.values():
+        print(i)
     return objlist
-
 
 
 def main():
